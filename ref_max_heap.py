2bc6420177a0bde47eb7d485a5fbd0c1d190ec79
Sabelan <uniquesingularity@gmail.com>
2024-03-03 23:44:44 -0800

    new file:   ref_max_heap.py

---
+++ ref_max_heap.py
@@ -0,0 +1,20 @@
+class RefMaxHeap:
+
+  def __init__(self):
+    self.heap = []
+    self.item_locations = {}
+
+  def popMax(self):
+    pass
+
+  def push(self, item):
+    pass
+
+  def modify(self, item, modify_fn):
+    pass
+
+  def _siftAwayFromRoot(self, index):
+    pass
+
+  def _siftTowardsRoot(self, index):
+    pass
